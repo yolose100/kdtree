a2568433421d9d2f5a5d5b1ab1d20a0e8b2083a3
io0_0 <elvis.tellez.1234@gmail.com>
2022-09-03 11:32:34 -0500

    kdree

--- js/kdtree.js
+++ js/kdtree.js
@@ -9,6 +9,42 @@ this.axis = axis;
 }
  }
 
- function getHeight ( node ) {}
+ function getHeight ( node ) {
+    if (!node) return 0 ;
+    return  Math.max(getHeight(node.left)+1, getHeight(node.right+1));
+
+ }
  function generate_dot ( node ) {}
- function build_kdtree ( points , depth = 0) {}
+ function build_kdtree(points, depth = 0){
+    var n = points.length;
+    var axis = depth % k;
+    
+    
+    if (n <= 0){
+    return null;
+    }
+    if (n == 1){
+    return new Node(points[0], axis)
+    }
+    
+    var median = Math.floor(points.length / 2);
+    
+    // sort by the axis
+    points.sort(function(a, b)
+    {
+    return a[axis] - b[axis];
+    });
+    //console.log(points);
+    
+    var left = points.slice(0, median);
+    var right = points.slice(median + 1);
+    
+    //console.log(right);
+    
+    var node = new Node(points[median].slice(0, k), axis);
+    node.left = build_kdtree(left, depth + 1);
+    node.right = build_kdtree(right, depth + 1);
+    
+    return node;
+    
+    }
--- js/sketch.js
+++ js/sketch.js
@@ -1,120 +1,33 @@
-/* 
-let qt;
-let count = 0;
-function setup() {
-    createCanvas(400, 400);
-
-    // centre point and half of width and height
-    let boundary = new Rectangle(200, 200, 200, 200);
-
-    // each leave just could have 4 elements
-    qt = new QuadTree(boundary, 4);
-
-    console.log(qt);
-    for (let i = 0; i < 200; i++) {
-        let p = new Point(Math.random() * 400, Math.random() * 400);
-        qt.insert(p);
+function setup () {
+    var width = 250;
+    var height = 200;
+    createCanvas (width , height ) ;
+    
+    background (0) ;
+    for (var x = 0; x < width; x += width / 10) {
+    
+    for (var y = 0; y < height; y += height / 5) {
+    stroke (125 , 125 , 125) ;
+    strokeWeight (1) ;
+    line (x, 0, x, height );
+    line (0 , y, width , y);
     }
-
-    background(0);
-    qt.show();
-}
- */
-
-let qt;
-let count = 0;
-let size = 700;
-let posx = 0;
-let posy = 0;
-let range = new Rectangle(posx,posy,30,30);
-
-function izquierda(){
-    posx = posx - 5;
-    range = new Rectangle(posx,posy,30,30);
-}
-
-function derecha(){
-    posx = posx + 5;
-    range = new Rectangle(posx,posy,30,30);
-}
-
-function arriba (){
-    posy = posy - 5;
-    range = new Rectangle(posx,posy,30,30);
-}
-
-function abajo(){
-    posy = posy + 5;
-    range = new Rectangle(posx,posy,30,30);
-}
-
-
-
-function resultQuery(){
-    range = new Box(minQuery,maxQuery);
-    let points = [];
-    tree.query(range,points);
-    if(points.length==0){
-        alert("No existen puntos en el range.");
-    }else{
-        for (let i = 0; i < points.length; i++) {
-            const element = points[i];
-            alert("("+Math.round(element.x,2)+","+Math.round(element.y,2)+","+Math.round(element.z,2)+")");
-        }
-    }            
-    console.log(points);
-    //tree.query()
-}
-function setup() {
-    let miCanvas = createCanvas(size, size);
-    miCanvas.parent("division");
-    let boundary = new Rectangle(size/2, size/2, size/2, size/2);
-    qt = new QuadTree(boundary, 4);
-}
-
-function draw() {
-    background(0);
-    if (mouseIsPressed) {
-        for (let i = 0; i < 1; i++) {
-            let m = new Point(mouseX + random(-5, 5), mouseY + random(-5, 5));
-            qt.insert(m)
-        }
     }
-    background(0);
-    qt.show();
-    if (typeof range == undefined ){
-        range = new Rectangle(random(-size/2,size/2),random(-size/2,size/2),30,30);
+    
+    var data = [];
+    for ( let i = 0; i < 12; i ++) {
+    var x = Math.floor ( Math.random () * height );
+    var y = Math.floor ( Math.random () * height );
+    data.push ([x, y]) ;
+    
+    fill (255 , 255 , 255) ;
+    circle (x, height - y, 7) ; // 200 -y para q se dibuje apropiadamente
+    textSize (8) ;
+    text (x + ',' + y, x + 5, height - y);// 200 -y para q se dibuje
+    
+    
+    
     }
-    stroke(0,0,255);
-
-    rect(range.x,range.y,range.w *2 , range.h *2);
-    let points = [];
-    qt.query(range,points);
-    for (let p of points ){
-        strokeWeight(4);
-        point(p.x,p.y);
-    }
-
-}
-function contar(){
-    let points = [];
-    qt.query(range,points);
-   
-    alert("en el rango hay "+points.length)
-  
-}
-
-
-
-
-function cuadrado(){
-    posx = random(size);
-    posy = random(size);
-    range = new Rectangle(posx,posy,30,30);
-    stroke(0,0,255);
-
-    rect(range.x,range.y,range.w *2 , range.h *2);
-    console.log("entra")
-}
-
-
+    
+    var root = build_kdtree ( data ) ;
+    console.log ( root );}

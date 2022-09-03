fda724d5478b7d4704740684705f90993eede3ad
Richard Cruz <ricktonycr@gmail.com>
2022-09-03 12:11:19 -0500

    Adding closest_point_brute_force and naive_closest_point.

--- js/kdtree.js
+++ js/kdtree.js
@@ -48,3 +48,53 @@ this.axis = axis;
     return node;
     
     }
+
+
+function distanceSquared ( point1 , point2 ){
+    var distance = 0;
+    for (var i = 0; i < k; i ++)
+        distance += Math.pow (( point1 [i] - point2 [i]) , 2) ;
+    return Math.sqrt ( distance );
+}
+
+function closest_point_brute_force ( points , point ) {
+    let min      = 999999999;
+    let minIndex = -1; 
+    for ( let i = 0 ; i < points.length ; i++ ) {
+        let temp = distanceSquared( point , points[ i ] );
+        if ( temp < min ) {
+            min = temp;
+            minIndex = i;
+        }
+    }
+    return (minIndex != -1 ) ? points[ minIndex ] : null;
+}
+
+function naive_closest_point (node , point , depth = 0, best = null ) {
+    if ( node == null || point == null )
+        return null;
+
+    let axis   = depth % k;
+    nodeValue  = node.point[ axis ];
+    pointValue = point[ axis ];
+
+    if ( best ) {
+        let d1 = distanceSquared ( best , point );
+        let d2 = distanceSquared ( node.point , point );
+
+        best = ( d1 < d2 ) ? best : node.point;
+    } else {
+        best = node.point;
+    }
+
+    if ( pointValue < nodeValue) {
+        if ( node.left )
+            return naive_closest_point( node.left , point , ++depth , best);
+
+    } else {
+        if ( node.right )
+            return naive_closest_point( node.right , point , ++depth , best);
+    }
+
+    return best;
+}
